9efe20fe984a82b4183daab1e36f58d5211caf79
Brice TEXIER <brice@codeur.com>
2026-01-19 15:37:05 +0100

    Adjust script.sh

--- script.sh
+++ script.sh
@@ -50,9 +50,9 @@ else
 fi
 
 echo '::group:: Running bundler-audit with reviewdog 🐶 ...'
-# shellcheck disable=SC2086
 ${BUNDLE_EXEC}bundler-audit update
 
+# shellcheck disable=SC2086
 ${BUNDLE_EXEC}bundler-audit check ${INPUT_BUNDLER_AUDIT_FLAGS} --format json --quiet \
   | ruby ${GITHUB_ACTION_PATH}/rdjson_formatter/rdjson_formatter.rb \
   | reviewdog -f=rdjson \
